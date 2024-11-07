2b29e656d37e4ef75da6dd5b72282ea4cc27c4a8
Agnieszka Trefler <hello@webscene.pl>
2024-11-07 15:16:07 +0100

    Add function sumOfNumbers to 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,11 @@
-const x = 5;
-let result = 0;
+const x = 6;
+
+const sumOfNumbers = (x) => {
+    let result = 0;
+    for (let i = 1; i <= x; i++) {
+        result = result + i;
+    }
+    return result;
+} 
+
+console.log(sumOfNumbers(x));
