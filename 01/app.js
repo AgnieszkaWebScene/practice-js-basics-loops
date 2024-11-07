6d7710b153619a1ce8083c7987d510e77283559a
Agnieszka Trefler <hello@webscene.pl>
2024-11-07 14:22:29 +0100

    Add function checkNumber to 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,16 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+
+const checkNumber = (randomNumber) => {
+    const limit = 5;
+    if (randomNumber > limit) {
+        for (let i = 0; i < randomNumber - limit + 1; i++) {
+            console.log( limit + i);
+        }
+    } else {
+        console.log('The number drawn is too small to use a loop');
+    }
+} 
+
+checkNumber(randomNumber);
